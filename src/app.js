e14aa5d45976059e262d8ee8e92280ae47e0d2f4
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-08-15 13:16:30 -0700

    Create error-handling middleware and add comments for clarity

--- src/app.js
+++ src/app.js
@@ -13,14 +13,28 @@ const app = express();
 app.set('views', __dirname);
 app.set('view engine', 'ejs');
 
+// Use middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+
+// Use error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  if (process.env.NODE_ENV === 'development') {
+    res.status(500).send(err.message);
+  } else {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Define the routes
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/member', membersRouter);
 app.use('/messages', messagesRouter);
 
-app.use(express.urlencoded({ extended: false }));
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
-app.use(passport.session());
-
+// Define the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
